675605abe12cdae7343a7f5688820b1e9299d465
Braden <bradenk04@proton.me>
2026-02-03 12:48:22 -0800

    fix: Fixed new config being made as dir

--- src/main/kotlin/io/github/bradenk04/claims/config/ConfigHandler.kt
+++ src/main/kotlin/io/github/bradenk04/claims/config/ConfigHandler.kt
@@ -25,7 +25,7 @@ object ConfigHandler {
         val languageStr = config.language
         val langFile = File(dataFolder, "lang/$languageStr.toml")
         if (!langFile.exists()) {
-            langFile.mkdirs()
+            langFile.parentFile.mkdirs()
             langFile.createNewFile()
             langFile.writeText(TomlFileReader.encodeToString(LanguageConfig.serializer(), LanguageConfig()))
         }
